ee96321ba9ea7ecd1f6c2b6b56d1d31755a791cd
Humaiza Naz <148262197+Humaiza-naz@users.noreply.github.com>
2024-05-01 06:42:34 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,50 @@
+#! /urs/bin/env node
+import inquirer from "inquirer";
+console.log(`Welcome To A Rock,Paper,Scissor Game`);
+let choice = await inquirer.prompt([
+  {
+    name: "choice1",
+    type: "list",
+    choices: ["Rock", "Paper", "Scissor"],
+  },
+  {
+    name: "choice2",
+    type: "list",
+    choices: ["Rock", "Paper", "Scissor"],
+  },
+]);
+console.log(`User one picked ${choice.choice1}`);
+console.log(`User two picked ${choice.choice2}`);
+let hello = function(choice1:string, choice2:string){
+    if (choice.choice1 === "Rock") {
+        if(choice.choice2 === "Scissor"){
+            console.log(`Rock Wins`);
+        }else if (choice.choice2 === "Rock"){
+            console.log(`Its a Tie`);
+        }else if(choice.choice2=== "Paper"){
+            console.log(`Paper Wins`);
+            
+        }}
+        else if(choice.choice1==="Paper"){
+            if(choice.choice2 === "Rock"){
+                console.log(`Paper Wins`);
+            } else if (choice.choice2 === "Paper"){
+                console.log(`Its a Tie`);
+            }else if(choice.choice2 === "Scissor"){
+                console.log("Scissor wins");
+            }
+        }else if(choice.choice2 === "Scissor"){
+            if (choice.choice2=== "Rock"){
+                console.log("Rock wins");
+                
+            }else if(choice.choice2 === "Paper"){
+                console.log("Scissor win");
+                
+            }else if(choice.choice2=== "Scissor"){
+                console.log(`Its a Tie`);
+                
+            }
+        }
+  
+}
+hello(choice.choice1,choice.choice2)
